1fa6edffd41f526e07f872766b3b2f7432fedb9a
Unguliant <ungulient@gmail.com>
2017-05-19 01:56:01 +0300

    Finished XMLParserTest.

--- book-test/src/test/java/il/ac/technion/cs/sd/book/test/XMLParserTest.java
+++ book-test/src/test/java/il/ac/technion/cs/sd/book/test/XMLParserTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.util.SortedMap;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -15,14 +14,14 @@ import static org.junit.Assert.assertTrue;
 
 public class XMLParserTest extends SdHw1Test{
 
-    private static SortedMap getSortedMapFromXML(String fileName) throws FileNotFoundException {
+    private static SortedMap getSortedMapFromXML(String fileName, boolean swapKeys) throws FileNotFoundException {
         String xml = getFilesContent("./xmlParserTest/" + fileName);
-        return XMLParser.parseXMLToSortedMap(xml, "/Root/Reviewer/Review",false);
+        return XMLParser.parseXMLToSortedMap(xml, "/Root/Reviewer/Review",swapKeys);
     }
 
     @Test
     public void shouldIgnoreDuplicatesAndGetLast() throws Exception {
-        SortedMap sortedMap = getSortedMapFromXML("duplicates.xml");
+        SortedMap sortedMap = getSortedMapFromXML("duplicates.xml", false);
 
         String reviewerAndBookId = String.join(",","1", "A");
         String score = "3";
@@ -36,18 +35,52 @@ public class XMLParserTest extends SdHw1Test{
 
     @Test
     public void shouldIgnoreEmptyReviewers() throws Exception {
+        SortedMap sortedMap = getSortedMapFromXML("small.xml", false);
 
+        assertTrue(sortedMap.containsKey("0,A"));
+        assertTrue(sortedMap.containsKey("1,A"));
+        assertTrue(sortedMap.containsKey("1,B"));
+        assertEquals(3, sortedMap.size());
+        //thus, doesn't contain reviewer with id=8
     }
 
     @Test
-    public void shouldSwapKeys() throws Exception {
+    public void shouldNotIgnoreSeparate() throws Exception {
+        SortedMap sortedMap = getSortedMapFromXML("separate.xml", false);
 
+        assertTrue(sortedMap.containsKey("1,A"));
+        assertTrue(sortedMap.containsKey("1,B"));
+        assertEquals(2, sortedMap.size());
     }
 
     @Test
     public void shouldSort() throws Exception {
+        SortedMap sortedMap = getSortedMapFromXML("sort.xml", false);
+        String[] expected = {"0", "1", "2"};
 
+        //thus sorted
+        assertTrue(Arrays.equals(expected, sortedMap.values().toArray()));
+
+        assertEquals("0", sortedMap.get("0,A"));
+        assertEquals("1", sortedMap.get("1,A"));
+        assertEquals("2", sortedMap.get("1,B"));
+
+        assertEquals(3, sortedMap.size());
     }
 
+    @Test
+    public void shouldSwapKeys() throws Exception {
+        SortedMap sortedMap = getSortedMapFromXML("sort.xml", true);
+        String[] expected = {"0", "1", "2"};
+
+        //thus sorted
+        assertTrue(Arrays.equals(expected, sortedMap.values().toArray()));
+
+        assertEquals("0", sortedMap.get("A,0"));
+        assertEquals("1", sortedMap.get("A,1"));
+        assertEquals("2", sortedMap.get("B,1"));
+
+        assertEquals(3, sortedMap.size());
+    }
 
 }
