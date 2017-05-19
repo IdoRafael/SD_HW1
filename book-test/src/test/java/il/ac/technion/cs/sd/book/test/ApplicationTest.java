892b5c30dbdf41990ba51440da8dee10c7773325
Unguliant <ungulient@gmail.com>
2017-05-19 03:51:19 +0300

    Added hugeTest to ApplicationTest.

--- book-test/src/test/java/il/ac/technion/cs/sd/book/test/ApplicationTest.java
+++ book-test/src/test/java/il/ac/technion/cs/sd/book/test/ApplicationTest.java
@@ -5,21 +5,20 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import il.ac.technion.cs.sd.book.app.BookScoreInitializer;
 import il.ac.technion.cs.sd.book.app.BookScoreReader;
-import il.ac.technion.cs.sd.book.ext.LineStorage;
 import il.ac.technion.cs.sd.book.ext.LineStorageFactory;
-import il.ac.technion.cs.sd.book.library.Storage;
 import org.junit.Test;
-import org.mockito.Mockito;
 
-import java.io.File;
+import java.awt.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.MessageFormat;
 import java.util.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static java.lang.Math.min;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
 
 
 public class ApplicationTest extends SdHw1Test {
@@ -51,10 +50,58 @@ public class ApplicationTest extends SdHw1Test {
     }
 
     @Test
-    public void testSimple() throws Exception {
+    public void testSimpleExampleTest() throws Exception {
         BookScoreReader reader = setupAndGetReader("small.xml");
+
         assertEquals(Arrays.asList("Boobar", "Foobar", "Moobar"), reader.getReviewedBooks("123"));
         assertEquals(OptionalDouble.of(6.0), reader.getScoreAverageForReviewer("123"));
         assertEquals(OptionalDouble.of(10.0), reader.getAverageReviewScoreForBook("Foobar"));
+    }
+
+
+/*    @Test
+    public void ido() throws Exception {
+        try{
+            PrintWriter writer = new PrintWriter("C:\\Users\\Ido\\Desktop\\asdf\\huge.xml", "UTF-8");
+            writer.println("<Root>\n");
+
+
+            IntStream.range(0, 100)
+                    .forEachOrdered(i -> {
+
+                        writer.println(new MessageFormat("  <Reviewer Id=\"{0}\">\n").format(new Object[] {i}));
+
+                        IntStream.range(0, 100)
+                                .forEachOrdered(j -> {
+                                            writer.println(new MessageFormat(
+                                                    "    <Review>\n" +
+                                                    "      <Id>{0}</Id>\n" +
+                                                    "      <Score>{1}</Score>\n" +
+                                                    "    </Review>\n").format(new Object[] {j, j}));
+                                        }
+                                        );
+
+                        writer.println("  </Reviewer>\n");
+                    });
+
+            writer.println("</Root>\n");
+
+            writer.close();
+        } catch (IOException e) {
+            // do something
+        }
+
+    }*/
+
+    @Test
+    public void hugeTest() throws Exception {
+        BookScoreReader reader = setupAndGetReader("huge.xml");
+
+        List<String> expected = IntStream.range(0,100).mapToObj(i -> "" + i).collect(Collectors.toList());
+        expected.sort(String::compareTo);
+
+        assertEquals(expected,reader.getReviewedBooks("5"));
+        assertEquals(OptionalDouble.of(49.5), reader.getScoreAverageForReviewer("5"));
+        assertEquals(OptionalDouble.of(50.0), reader.getAverageReviewScoreForBook("50"));
     }
 }
